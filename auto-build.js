4e7d136369c3a062157bd10cf5147db5a24c1279
InteractiveRob <rkirkner@mail.com>
2018-09-02 22:42:37 -0400

    teardown r11

--- auto-build.js
+++ auto-build.js
@@ -13,12 +13,12 @@ app.post('/public/', function (req, res) {
   res.send(`You sent: ${body} to Express`)
 })
 
-// Tell our app to listen on port 3000
-app.listen(3000, function (err) {
+// Tell our app to listen on port ${PORT?}
+app.listen(${PORT?}, function (err) {
   if (err) {
     throw err
   }
 
-  console.log('Server started on port 3000')
+  console.log('Server started on port' + ${PORT?})
 })
 console.log("i am here");
