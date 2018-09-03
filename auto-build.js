998238f77cc5d621b3162b51c2fd165acc19b769
InteractiveRob <rkirkner@mail.com>
2018-09-03 12:50:39 -0400

    build automation r7

--- auto-build.js
+++ auto-build.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 // Create a new instance of express
 const app = express()
 const port = process.env.PORT || 3000;
+const exec = require('child_process').exec, child;
+
 
 // Tell express to use the body-parser middleware and to not parse extended bodies
 const json_body_parser = bodyParser.json();
@@ -15,10 +17,29 @@ app.use(urlencoded_body_parser);
 // Route that receives a POST request to /sms
 app.post('/', function (req, res) {
   const body = req.body
-  let name = body.data.name
-  res.set('Content-Type', 'text/plain')
-  res.send(`You sent: ${name} to Express`)
+  let build = body.data.build
   console.log(`You sent: ${body} to Express`)
+  
+  if(build == 'go'){
+	res.set('Content-Type', 'text/plain')
+	res.send(`Build running`)	
+	
+	//build script
+	const testscript = exec('npm run patch & gatsby build & sftp-deploy');
+  
+	testscript.stdout.on('data', function(data){
+	    console.log(data); 
+	    // sendBackInfo();
+	});
+	
+	testscript.stderr.on('data', function(data){
+	    console.log(data);
+	    // triggerErrorStuff(); 
+	});	   
+  }
+  else{
+	'FAIL build not running'
+  }
 })
 
 // Tell our app to listen on port 3000
