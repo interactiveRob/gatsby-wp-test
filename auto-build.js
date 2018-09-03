c9e9f6aa59e1c5e3adde983ecc9a0177cfbc2fdc
InteractiveRob <rkirkner@mail.com>
2018-09-03 12:55:17 -0400

    build automation r8

--- auto-build.js
+++ auto-build.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 // Create a new instance of express
 const app = express()
 const port = process.env.PORT || 3000;
-const exec = require('child_process').exec, child;
+var exec = require('child_process').exec;
 
 
 // Tell express to use the body-parser middleware and to not parse extended bodies
