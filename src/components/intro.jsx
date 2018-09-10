45ff12899e78c8609171fc63fef5058efc31ef99
Kirkner <rkirkner@mail.com>
2018-09-10 14:49:58 -0400

    adding resources -- content population

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import blueSVG from "../layouts/img/blue1.svg";
 import circleDown from "../layouts/img/circle-down.svg";
+import pdfDownload from "../layouts/img/CON_9207_Energy_Purchasing_Strategies_White_Paper Sept2018.pdf"
 
 class Intro extends React.Component {
   render() {
@@ -13,10 +14,10 @@ class Intro extends React.Component {
 	                </div>
 	                <p className="add-slide-up">With Constellation, you can optimize your power purchasing strategy by customizing the components of price, time and execution. In a typically volatile power market, a flexible purchasing strategy can help you achieve budget certainty and mitigate risk.</p>
 	                <p className="add-slide-up">Learn more by exploring the information below, or see how different purchasing strategies perform over time by reading the white paper, "<a href="#">Evaluating Power Purchasing Strategies for Your Business</a>."</p>
-	                <button className="add-pop">
+	                <a href={pdfDownload} target="_blank" className="add-pop btn">
 	                    <span>Download now</span>
 	                    <object data={circleDown} type="image/svg+xml" style={{maxWidth : 20}}/>
-	                </button>
+	                </a>
 	            </div>
 	        </div>
 	    </section>		 
