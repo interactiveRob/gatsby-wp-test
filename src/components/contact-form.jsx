b30e26c45d1ad60a6de70b868f19a0f20d6f60b3
InteractiveRob <rkirkner@mail.com>
2018-09-10 01:20:45 -0400

    form validation, style changes

--- src/components/contact-form.jsx
+++ src/components/contact-form.jsx
@@ -3,50 +3,60 @@ import React from 'react';
 class ContactForm extends React.Component {
 	 constructor(props){
 	    super(props);
-	    this.state = { textValue: "" };
+	    this.state = { 
+		    firstNameValue: "",
+		    lastNameValue: "",
+		    companyValue: "",
+		    jobTitleValue: "",
+		    emailValue: "",
+		    phoneValue: ""
+		    
+		 };
 	  }
 	changeText(event){
 		  this.setState({
-			
+		  	[event.target.id]: event.target.value
 		  })
 	}			
   render() {	 
 	 return(		 
        <div className="popup-holder">
-           <div className="container">
-				
-<form acceptCharset="UTF-8" method="post" action="https://events.constellation.com/l/484581/2018-09-08/7tsvj" className="form" id="pardot-form">
+       		<div className="container">
+          		<h2 className="form-header">Let’s Talk</h2> 
+		  		<div className="thank-you-msg text hide-me" dangerouslySetInnerHTML = {{__html : '<strong>Thank you from Constellation!</strong> <br/> We appreciate your interest in finding a better strategy for purchasing power. Someone from Constellation will be in touch with you soon.'}} />		  			
+<form acceptCharset="UTF-8" method="post" action="https://events.constellation.com/l/484581/2018-09-08/7tsvj" className="form show-me" id="pardot-form">
 	<div className="form-field  first_name pd-text required    ">
-		<label className="field-label" htmlFor="484581_33987pi_484581_33987">First Name</label>
-		<input value={this.state.firstNameValue} onChange = {this.changeText.bind(this)} type="text" name="484581_33987pi_484581_33987" id="484581_33987pi_484581_33987"  className="text" size="30" maxLength="40" />
+		<label className="field-label" htmlFor="484581_33987pi_484581_33987">First Name*</label>
+		<input value={this.state.firstNameValue} onChange = {this.changeText.bind(this)} type="text" name="484581_33987pi_484581_33987" id="firstNameValue"  className="text" size="30" maxLength="40" />
 		</div>
 		<div className="form-field  last_name pd-text required    ">
-			<label className="field-label" htmlFor="484581_33989pi_484581_33989">Last Name</label>
-			<input value={this.state.lastNameValue} onChange = {this.changeText.bind(this)} type="text" name="484581_33989pi_484581_33989" id="484581_33989pi_484581_33989"  className="text" size="30" maxLength="80" />
+			<label className="field-label" htmlFor="484581_33989pi_484581_33989">Last Name*</label>
+			<input value={this.state.lastNameValue} onChange = {this.changeText.bind(this)} type="text" name="484581_33989pi_484581_33989" id="lastNameValue"  className="text" size="30" maxLength="80" />
 			</div>
 			<div className="form-field  company pd-text required    ">
-				<label className="field-label" htmlFor="484581_33991pi_484581_33991">Company</label>
-				<input value={this.state.companyValue} onChange = {this.changeText.bind(this)} type="text" name="484581_33991pi_484581_33991" id="484581_33991pi_484581_33991"  className="text" size="30" maxLength="255" />
+				<label className="field-label" htmlFor="484581_33991pi_484581_33991">Company*</label>
+				<input value={this.state.companyValue} onChange = {this.changeText.bind(this)} type="text" name="484581_33991pi_484581_33991" id="companyValue"  className="text" size="30" maxLength="255" />
 				</div>
 				<div className="form-field  job_title pd-text required    ">
-					<label className="field-label" htmlFor="484581_33993pi_484581_33993">Job Title</label>
-					<input value={this.state.jobTitleValue} onChange = {this.changeText.bind(this)} type="text" name="484581_33993pi_484581_33993" id="484581_33993pi_484581_33993"  className="text" size="30" maxLength="128" />
+					<label className="field-label" htmlFor="484581_33993pi_484581_33993">Job Title*</label>
+					<input value={this.state.jobTitleValue} onChange = {this.changeText.bind(this)} type="text" name="484581_33993pi_484581_33993" id="jobTitleValue"  className="text" size="30" maxLength="128" />
 					</div>
 					<div className="form-field  email pd-text required    ">
-						<label className="field-label" htmlFor="484581_33995pi_484581_33995">Email</label>
-						<input value={this.state.emailValue} onChange = {this.changeText.bind(this)} type="text" name="484581_33995pi_484581_33995" id="484581_33995pi_484581_33995"  className="text" size="30" maxLength="255"/>
+						<label className="field-label" htmlFor="484581_33995pi_484581_33995">Email*</label>
+						<input value={this.state.emailValue} onChange = {this.changeText.bind(this)} type="text" name="484581_33995pi_484581_33995" id="emailValue"   className="text" size="30" maxLength="255"/>
 						</div>
 						<div className="form-field  phone pd-text     ">
 							<label className="field-label" htmlFor="484581_33997pi_484581_33997">Phone Number</label>
-							<input value={this.state.phoneValue} onChange = {this.changeText.bind(this)} type="text" name="484581_33997pi_484581_33997" id="484581_33997pi_484581_33997"  className="text" size="30" maxLength="40" />
+							<input value={this.state.phoneValue} onChange = {this.changeText.bind(this)} type="text" name="484581_33997pi_484581_33997" id="phoneValue"  className="text" size="30" maxLength="40" />
 							</div>
 	
 						<div className="submit blue">
-							<input value={this.state.textValue} onChange = {this.changeText.bind(this)} type="submit" id="form-submit" accessKey="s" 	/>
+							<input value="Submit" onChange = {this.changeText.bind(this)} type="submit" id="form-submit" accessKey="s" 	/>
 							</div>
 						</form>		   		
-
-               <button className="bg-oj close-chart">Close</button>
+				<div className="close-btn">
+	               <button className="bg-oj close-chart">Close</button>
+	            </div>   
            </div>
        </div>	 		 
 	 )
--- src/layouts/js/logic.js
+++ src/layouts/js/logic.js
@@ -78,9 +78,93 @@ function form_submit(){
 	console.log('request sent');
 }
 
-document.querySelector('#form-submit').addEventListener('click',function(e){
+function validate_form(){
+	//error messages
+	let req_msg = '<span class="error"> --This field is required.</span>'
+	let email_msg = '<span class="error"> --Invalid email address.</span>'
+
+	let valid = false;
+	if(!document.querySelector('.error')){
+		valid = true;
+	}
+	
+	document.querySelectorAll('#pardot-form .form-field').forEach(formInput =>{
+		let this_input = formInput.querySelector('input');
+		let this_id = this_input.getAttribute('id');
+		let this_label = formInput.querySelector('.field-label');				
+		
+		let test_inputs = function(e){
+			let this_value = this_input.getAttribute('value');
+
+			//refresh errors
+			let this_error = formInput.querySelector('.error');
+			if (this_error){
+				this_error.remove();
+			}
+			
+			if(this_value.length == 0 && formInput.classList.contains('required')){
+				this_label.insertAdjacentHTML('beforeend', req_msg);
+			}
+			else if(this_input.getAttribute('id') == 'emailValue') {
+				if(this_value.indexOf('@') == -1 || this_value.indexOf('.') == -1 ){
+					this_label.insertAdjacentHTML('beforeend', email_msg);					
+				}
+			}
+			else{
+				//nothing
+				//console.log(this_id + ' ' + this_input.getAttribute('value').length);
+				//console.log( this_id + ' validation passed');
+			}
+		}
+		
+		this_input.addEventListener('change', test_inputs);
+		this_input.addEventListener('blur', test_inputs);
+	});
+	
+	//if there are no error messages, allow the form to be submitted
+	return valid; 
+}
+
+validate_form();		
+
+function thank_you_msg(){
+	let contact_form = document.querySelector('#pardot-form');
+	let form_header = document.querySelector('.form-header'); 
+	let thank_you_msg = document.querySelector('.thank-you-msg'); 
+	
+	contact_form.classList.remove('show-me');
+	contact_form.classList.add('hide-me');
+	form_header.classList.add('hide-me');
+	
+	thank_you_msg.classList.remove('hide-me');
+	thank_you_msg.classList.add('show-me');
+}
+
+document.querySelector('#form-submit').addEventListener('click', function(e){
 	e.preventDefault();
-	form_submit();
+
+	//triggers a 'change' event on all form inputs so they validate when [Submit] is clicked
+
+	document.querySelectorAll('#pardot-form input').forEach(input =>{
+		if ("createEvent" in document) {
+		    var evt = document.createEvent("HTMLEvents");
+		    evt.initEvent("change", false, true);
+		    input.dispatchEvent(evt);
+		}
+		else{
+		    input.fireEvent("onchange");		
+		}
+	});
+	
+	if(validate_form()){
+		console.log('submit validation passed');
+		//form_submit();		
+		thank_you_msg();
+	} else{
+		console.log('submit validation falied');
+	}
+		
+	
 });
 
 
